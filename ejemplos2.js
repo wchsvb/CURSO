d472c4c3cd829bf9bd330ecfa205213f8a6db53f
wchsvb <al24510561@tapachula.tecnm.mx>
2025-10-27 17:02:46 -0600

    Añade archivos de ejemplo js

---
+++ ejemplos2.js
@@ -0,0 +1,27 @@
+// tipos de manejode de variables
+
+// let y const
+let name = "victor manuel ";
+let lastName = "lorenzana Guzman";
+
+const fullName = name + " " + lastName;
+
+console.log(fullName);
+
+//boleneans
+
+let x= true
+let y= false
+
+//objetos
+const persona = {
+    firstName: "victor manuel",lastName: "lorenzana guzman", age: 28};
+ console.log(persona.firstName);  
+
+ // arreglos
+ const frutas = ["manzana", "platano", "cereza", "fresa"];
+ console.log(frutas[1]);
+
+ //date objeect
+ const hoy = new Date();
+ console.log(hoy);
